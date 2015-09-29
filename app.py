4cf2d574d45fa91f054e9f0fb42373a9031183b6
Julius Carvajal <juliuscarvajal21@gmail.com>
2015-09-29 10:18:53 +1000

    fix bug

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def get_player_window():
   EnumWindows(EnumWindowsProc(foreach_window), 0)
 
 def player_loop():
+    global player_hwnd
     while True:
       player = run_player()
       print "Player", player.pid, "has run..."
