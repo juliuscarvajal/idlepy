1ced65beaeceec54697e0cfeeea785fae517641a
Julius Carvajal <juliuscarvajal21@gmail.com>
2016-01-29 16:50:24 +1100

    added installer

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ app_name = 'app.exe'
 os.system('taskkill /f /im ' + app_name)
 
 setup(
+  name='Switcheroo',
+  version='0.1',
+  description='A background app that switches to the player when the kiosk is idle and restores the kiosk app once the kiosk gains user interaction.',
+  author='Julius Carvajal',
+  author_email='juliuscarvajal21@gmail.com',
   options = {'py2exe' : { 'optimize' : 2}},
   windows = [{'script' : "app.py"}],
   data_files = [('', ['config.ini', 'user.ini', 'forever.bat'])],
